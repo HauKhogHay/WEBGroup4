4cc96aed89f3ed01789194a2be008fd238151e3d
HauKhogHay <haudaica36@gmail.com>
2021-10-27 20:08:43 +0700

    first commit

---
+++ EmployeeManagement.Api/Models/AppDbContext.cs
@@ -0,0 +1,84 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Seed Departments Table
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 1, DepartmentName = "IT" });
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 2, DepartmentName = "HR" });
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 3, DepartmentName = "Payroll" });
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 4, DepartmentName = "Admin" });
+
+            // Seed Employee Table
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "SP1",
+                LastName = "",
+                Email = "David@pragimtech.com",
+                DateOfBrith = new DateTime(1980, 10, 5),
+                Gender = Gender.Male,
+                DepartmentId = 1,
+                PhotoPath = "images/sp1.png"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "SP2",
+                LastName = "",
+                Email = "Sam@pragimtech.com",
+                DateOfBrith = new DateTime(1981, 12, 22),
+                Gender = Gender.Male,
+                DepartmentId = 2,
+                PhotoPath = "images/sp2.jpg"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "SP3",
+                LastName = "",
+                Email = "mary@pragimtech.com",
+                DateOfBrith = new DateTime(1979, 11, 11),
+                Gender = Gender.Female,
+                DepartmentId = 1,
+                PhotoPath = "images/sp3.png"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 4,
+                FirstName = "SP4",
+                LastName = "",
+                Email = "sara@pragimtech.com",
+                DateOfBrith = new DateTime(1982, 9, 23),
+                Gender = Gender.Female,
+                DepartmentId = 3,
+                PhotoPath = "images/sp4.png"
+            });
+        }
+    }
+}
